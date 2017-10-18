52c9f43f3457f7213242a26532b36c5232d3a840
Jan Kalfus <410455@mail.muni.cz>
2017-10-18 12:28:42 +0200

    Fixed addCarToStorage method, added sellCar method

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -28,9 +29,20 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 				&& car.getPrice() < referenceCar.getPrice()).collect(Collectors.toList());
 	}
 
-	@Override
+    @Override
 	public void addCarToStorage(Car car) {
+		if (car.getPrice() < 0) {
+			throw new IllegalArgumentException();
+		}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
+
+    @Override
+    public void sellCar(Car car) throws RequestedCarNotFoundException {
+        List<Car> carsForType = CarShopStorage.getInstancce().getCars().remove(car.getType());
+        if (carsForType == null || !carsForType.remove(car)) {
+            throw new RequestedCarNotFoundException("Car was not found");
+        }
+    }
 
 }
